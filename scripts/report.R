d0ec096bfbb9f0ff20c22d2aff2fc5dad2914e58
Taavi Päll <tapa741@gmail.com>
2023-11-28 16:35:37 +0200

    updated report.R script

--- scripts/report.R
+++ scripts/report.R
@@ -1,8 +1,29 @@
+#' ---
+#' title: Penis size vs reporting method
+#' date: 2023-11-28
+#' ---
+
+#'  
+#+
 library(tidyverse)
 library(here)
 
-# Penis measurements (and self-reported data) from various sources across many countries and regions.
-# by Lukas Burk
+
+#' 
+#' Penis measurements (and self-reported data) from various sources across many countries and regions
+#' by Lukas Burk.
+#' Importing data.
+#+
 path <- "data/penis_long_format.csv"
-read_csv(here(path))
-  
+penis <- read_csv(here(path))
+
+#' 
+#' Plotting penis size versus reporting @fig-pplot.
+#+
+#| label: fig-pplot
+#| fig-cap: "Well somebody is exaggerating their member size."
+penis %>% 
+  ggplot(aes(Method, length)) +
+  geom_boxplot() +
+  geom_point(position = "jitter")
+
